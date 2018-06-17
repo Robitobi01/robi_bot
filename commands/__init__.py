216a8071d300ae5aba687e63bd8f8bc69204d1cc
veirden <veirden@gmail.com>
2018-06-17 15:26:03 -0400

    Update __init__.py

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ["baseCommand", "commandCache", "hardwareCommand", "helpCommand", "listCommand", "reloadCommand", "scoreboardCommand", "statCommand", "structureCommand", "tpsCommand"]
+__all__ = ["baseCommand", "commandCache", "hardwareCommand", "helpCommand", "listCommand", "reloadCommand", "scoreboardCommand", "statCommand", "structureCommand", "tpsCommand", "worldsizeCommand"]
