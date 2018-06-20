bec6c203a25cb4602726938aa588725d11a9aa65
veirden <veirden@gmail.com>
2018-06-19 22:48:18 -0400

    Update hardwareCommand.py

--- commands/hardwareCommand.py
+++ commands/hardwareCommand.py
@@ -5,17 +5,17 @@ import psutil
 class HardwareCommand(BaseCommand):
     command_text = "!!hardware"
 
-    def __init__(self, discord, client, message, command_cache):
-        super().__init__(discord, client, message, command_cache)
+    def __init__(self, bot, command_cache):
+        super().__init__(bot, command_cache)
 
     def help(self):
         return '`' + self.command_text + '`  **-**  Displays currently used hardware recources\n'
 
-    async def process(self, args):
+    async def process(self, message, args):
         cpu_percentage = str(psutil.cpu_percent(interval = 0.5))
         ram_usage = str(round(psutil.virtual_memory()[3] / 1073741824, 2))
 
-        if self.client == None:
-            print('The current CPU usage is: **' + cpu_percentage + '%**\nThe current RAM usage is: **' + ram_usage + 'GB** ')
+        if self.bot:
+            await self.bot.send_message(message.channel, 'The current CPU usage is: **' + cpu_percentage + '%**\nThe current RAM usage is: **' + ram_usage + 'GB** ')
         else:
-            await self.client.send_message(self.message.channel, 'The current CPU usage is: **' + cpu_percentage + '%**\nThe current RAM usage is: **' + ram_usage + 'GB** ')
+            print('The current CPU usage is: **' + cpu_percentage + '%**\nThe current RAM usage is: **' + ram_usage + 'GB** ')
