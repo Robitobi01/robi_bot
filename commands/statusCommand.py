fd691ecb996d37aa2b126156ad499bf49c2d1312
Robitobi01 <robitobi01@gmx.de>
2018-06-24 20:50:22 +0200

    added status.py

---
+++ commands/statusCommand.py
@@ -0,0 +1,34 @@
+from .baseCommand import BaseCommand
+from utils import *
+
+import json
+import urllib.request
+
+import discord
+
+class StatusCommand(BaseCommand):
+    command_text = "!!status"
+
+    def __init__(self, bot, command_cache):
+        super().__init__(bot, command_cache)
+
+
+    def help(self):
+        return '`' + self.command_text + '`  **-**  Displays the current status of the Mojang services\n'
+
+    async def process(self, message, args):
+        api_url = 'https://status.mojang.com/check'
+        api_request = urllib.request.urlopen(urllib.request.Request(api_url)).read()
+        api_request = json.loads(api_request.decode("utf-8"))
+        status = dict()
+        for items in api_request:
+            for key, value in items.items():
+                if value == 'green': value = ':white_check_mark:'
+                if value == 'yellow': value = ':warning:'
+                if value == 'red': value = ':x:'
+                status[key] = value
+        em = generate_embed_table(list(status.keys()), list(status.values()))
+        em.set_author(
+            name = 'Mojang Status',
+            icon_url = 'https://cdn.discordapp.com/icons/336592624624336896/31615259cca237257e3204767959a967.png')
+        await self.bot.send_message(message.channel, embed = em)
