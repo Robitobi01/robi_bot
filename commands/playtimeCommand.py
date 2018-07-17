272f8d048b1586ba0880bfb234a6a8e62552949b
veirden <veirden@gmail.com>
2018-07-17 18:14:16 -0400

    reworked embed formatting

--- commands/playtimeCommand.py
+++ commands/playtimeCommand.py
@@ -28,8 +28,16 @@ def daysOfYear(days):
     return days % 365
 
 def formatNameForEmbed(name):
-    # The only special character allowed in Minecraft username is '_'
+    # The only markdown special character allowed in Minecraft username is '_'
     return name.replace('_', '\\_')
+
+def formatPlaytimeForEmbed(years, days, hours, minutes):
+    if years + days + hours + minutes == 0:
+        return ''
+
+    buffer = '' if years <= 0 else '{0}y '.format(years)
+    
+    return buffer + '{0:0>3}.{1:0>2}:{2:0>2}'.format(days, hours, minutes)
 
 class PlayerInfo:
     """Contains playtime information about a player."""
@@ -39,29 +47,22 @@ class PlayerInfo:
 
         self.played_minutes = 0
         self.played_days = 0
-        self.years = ''
-        self.days = ''
-        self.hours = ''
-        self.minutes = ''
+        self.years = 0
+        self.days = 0
+        self.hours = 0
+        self.minutes = 0
 
     def setPlaytime(self, ticks):
         self.played_minutes = ticksToMinutes(ticks)
         self.played_days = minutesToDays(self.played_minutes)
 
-        value = daysToYears(self.played_days)
-        self.years = str(value) if value > 0 else ''
-                                
-        value = daysOfYear(self.played_days)
-        self.days = str(value) if value > 0 else ''
-
-        value = minutesToHours(self.played_minutes)
-        self.hours = str(value) if value > 0 else ''
-
-        self.minutes = str(minutesOfHour(self.played_minutes)) if self.played_minutes > 0 else ''
+        self.years = daysToYears(self.played_days)
+        self.days = daysOfYear(self.played_days)
+        self.hours = minutesToHours(self.played_minutes)
+        self.minutes = minutesOfHour(self.played_minutes)
 
 class PlaytimeCommand(BaseCommand):
     command_text = "!!playtime"
-    divider_player_name = '========================='
 
     def __init__(self, bot, command_cache, survival_folder, stat_folder):
         super().__init__(bot, command_cache)
@@ -73,58 +74,28 @@ class PlaytimeCommand(BaseCommand):
         return ('`' + self.command_text + ' [<user_name> [<user_name> ...]]`  **-**  Shows playtime for players\n'
                 '`' + self.command_text + ' server`  **-**  Shows survival world times\n')
 
-    async def process(self, message, args):
-        if len(args) == 0:
-            try:
-                players = []
-                stat_id = 'stat.playOneMinute'
-                total_minutes = 0
-                total_ticks = 0
+    async def processPlayers(self, players):
+        try:
+            stat_id = 'stat.playOneMinute'
+            total_ticks = 0
 
-                async with CommandCache.semaphore:
-                    for item in zip(self.cache.names, self.cache.uuids):
-                        players.append(PlayerInfo(item[0], convert_uuid(item[1])))
-                
-                for player in players:
-                    file = os.path.join(self.stat_folder, player.uuid + '.json')
+            for player in players:
+                file = os.path.join(self.stat_folder, player.uuid + '.json')
 
-                    if not os.path.exists(file):
-                        continue
+                if not os.path.exists(file):
+                    continue
 
-                    with open(file) as json_data:
-                        stats = json.load(json_data)
+                with open(file) as json_data:
+                    stats = json.load(json_data)
 
-                    if not stat_id in stats:
-                        continue
+                if not stat_id in stats:
+                    continue
 
-                    stat_value = stats[stat_id]
+                stat_value = stats[stat_id]
 
-                    player.setPlaytime(stat_value)
+                player.setPlaytime(stat_value)
 
-                    total_ticks += stat_value
-
-                if players != []:
-                    total_minutes = ticksToMinutes(total_ticks)
-    
-                    # Sort players in descending order based on total playtime in minutes.
-                    players = sorted(players, key = lambda p: p.played_minutes, reverse = True)
-
-                    temp_player = PlayerInfo(PlaytimeCommand.divider_player_name, '')
-                    temp_player.setPlaytime(0)
-
-                    players.append(temp_player)
-
-                    temp_player = PlayerInfo('Total', '')
-                    temp_player.setPlaytime(total_ticks)
-
-                    players.append(temp_player)
-            except:
-                if self.bot:
-                    await self.bot.send_message(message.channel, 'No playerfile or stat found')
-                else:
-                    print('No playerfile or stat found')
-
-                return
+                total_ticks += stat_value
 
             if players == []:
                 if self.bot:
@@ -132,60 +103,73 @@ class PlaytimeCommand(BaseCommand):
                 else:
                     print('No playerfile or stat found')
             else:
+                # Sort players in descending order based on total playtime in minutes.
+                players = sorted(players, key = lambda p: p.played_minutes, reverse = True)
+
+                total_info = PlayerInfo('', '')
+                total_info.setPlaytime(total_ticks)
+
                 if self.bot:
-                    player_names, years, days, hours, minutes = zip(*list((formatNameForEmbed(p.player_name), p.years, p.days, p.hours, p.minutes) for p in players))
+                    player_names, playtimes = zip(*list((formatNameForEmbed(p.player_name), formatPlaytimeForEmbed(p.years, p.days, p.hours, p.minutes)) for p in players))
 
                     em = discord.Embed(
                         description = '',
                         colour = 0x003763)
 
                     em.add_field(
-                        name = 'Players',
+                        name = 'Player',
                         inline = True,
                         value = '\n'.join(player_names))
                     em.add_field(
-                        name = 'Years',
+                        name = 'Playtime',
                         inline = True,
-                        value = '\n'.join(years))
-                    em.add_field(
-                        name = 'Days',
-                        inline = True,
-                        value = '\n'.join(days))
-                    em.add_field(
-                        name = 'Hours',
-                        inline = True,
-                        value = '\n'.join(hours))
-                    em.add_field(
-                        name = 'Minutes',
-                        inline = True,
-                        value = '\n'.join(minutes))
+                        value = '\n'.join(playtimes))
                     
                     em.set_author(
                         name = 'Total Playtime', 
                         icon_url = 'https://cdn.discordapp.com/icons/336592624624336896/31615259cca237257e3204767959a967.png')
-                    em.set_footer(text = 'Total: {:,} minutes'.format(total_minutes))
+
+                    if len(players) > 1:
+                        em.set_footer(text = 'Total: {0}'.format(formatPlaytimeForEmbed(total_info.years, total_info.days, total_info.hours, total_info.minutes)))
 
                     await self.bot.send_message(message.channel, embed = em)
                 else:
-                    print('Total Playtime')
-                    print('Name: Years, Days, Hours, Minutes')
+                    print('Player: Playtime')
 
                     for p in players:
-                        if p.player_name == PlaytimeCommand.divider_player_name:
-                            print(p.player_name)
-                        else:
-                            print(p.player_name + ': ' + p.years + ', ' + p.days + ', ' + p.hours + ', ' + p.minutes)
-                    
-                    print('Total: {:,} minutes'.format(total_minutes))
+                        print(p.player_name + ': ' + formatPlaytimeForEmbed(p.years, p.days, p.hours, p.minutes))
+
+                    if len(players) > 1:
+                        print('Total: {0}'.format(formatPlaytimeForEmbed(total_info.years, total_info.days, total_info.hours, total_info.minutes)))
+        except:
+            if self.bot:
+                await self.bot.send_message(message.channel, 'No playerfile or stat found')
+            else:
+                print('No playerfile or stat found')
+
+    async def process(self, message, args):
+        if len(args) == 0:
+            # Process all players
+            try:
+                players = []
+
+                async with CommandCache.semaphore:
+                    for item in zip(self.cache.names, self.cache.uuids):
+                        players.append(PlayerInfo(item[0], convert_uuid(item[1])))
+
+                await self.processPlayers(players)
+            except:
+                if self.bot:
+                    await self.bot.send_message(message.channel, 'No playerfile or stat found')
+                else:
+                    print('No playerfile or stat found')
         elif len(args) == 1 and args[0] == 'server':
+            # Process server times
             try:
                 nbt_data = nbt.NBTFile(os.path.join(self.survival_folder, 'level.dat'))['Data']
                 
                 text_names = []
-                text_years = []
-                text_days = []
-                text_hours = []
-                text_minutes = []
+                text_times = []
 
                 data_time = nbt_data['Time'].value
                 data_daytime = nbt_data['DayTime'].value
@@ -196,49 +180,50 @@ class PlaytimeCommand(BaseCommand):
                 total_minutes = ticksToMinutes(data_time)
                 total_days = minutesToDays(total_minutes)
 
-                value = daysToYears(total_days)
-                text_years.append(str(value) if value > 0 else '')
-                
-                value = daysOfYear(total_days)
-                text_days.append(str(value) if value > 0 else '')
+                years = daysToYears(total_days)
+                days = daysOfYear(total_days)
+                hours = minutesToHours(total_minutes)
+                minutes = minutesOfHour(total_minutes)
 
-                value = minutesToHours(total_minutes)
-                text_hours.append(str(value) if value > 0 else '')
+                text_times.append(formatPlaytimeForEmbed(years, days, hours, minutes))
 
-                text_minutes.append(str(minutesOfHour(total_minutes)))
+                # Total Time In-Game
+                text_names.append('Total Time (IG)')
 
-                # World Time In-Game
-                text_names.append('World Time (IG)')
+                total_minutes = ticksToMinutes(data_time)
+                total_days = data_time // 24000 # DayTime is in ticks, 24000 = 1 Minecraft Day
 
-                world_days = data_daytime // 24000 # DayTime is in ticks, 24000 = 1 Minecraft Day
+                years = daysToYears(total_days)
+                days = daysOfYear(total_days)
+                hours = data_time % 24000 // 1000 # 1 hour in-game is 1000 ticks
+                minutes = 0
 
-                value = daysToYears(world_days)
-                text_years.append(str(value) if value > 0 else '')
-                
-                value = daysOfYear(world_days)
-                text_days.append(str(value) if value > 0 else '')
-
-                value = data_daytime % 24000 // 1000 # 1 hour in-game is 1000 ticks
-                text_hours.append(str(value) if value > 0 else '')
-
-                text_minutes.append('0')
+                text_times.append(formatPlaytimeForEmbed(years, days, hours, minutes))
                 
                 # World Time IRL
                 text_names.append('World Time (IRL)')
                 
                 world_minutes = ticksToMinutes(data_daytime)
                 world_days = minutesToDays(world_minutes)
-
-                value = daysToYears(world_days)
-                text_years.append(str(value) if value > 0 else '')
                 
-                value = daysOfYear(world_days)
-                text_days.append(str(value) if value > 0 else '')
+                years = daysToYears(world_days)
+                days = daysOfYear(world_days)
+                hours = minutesToHours(world_minutes)
+                minutes = minutesOfHour(world_minutes)
 
-                value = minutesToHours(world_minutes)
-                text_hours.append(str(value) if value > 0 else '')
+                text_times.append(formatPlaytimeForEmbed(years, days, hours, minutes))
 
-                text_minutes.append(str(minutesOfHour(world_minutes)))
+                # World Time In-Game
+                text_names.append('World Time (IG)')
+
+                world_days = data_daytime // 24000 # DayTime is in ticks, 24000 = 1 Minecraft Day
+
+                years = daysToYears(world_days)
+                days = daysOfYear(world_days)
+                hours = data_daytime % 24000 // 1000 # 1 hour in-game is 1000 ticks
+                minutes = 0
+
+                text_times.append(formatPlaytimeForEmbed(years, days, hours, minutes))
             except:
                 if self.bot:
                     await self.bot.send_message(message.channel, 'No level data found')
@@ -255,23 +240,11 @@ class PlaytimeCommand(BaseCommand):
                 em.add_field(
                     name = 'Name',
                     inline = True,
-                    value = text_names)
+                    value = '\n'.join(text_names))
                 em.add_field(
-                    name = 'Years',
+                    name = 'Time',
                     inline = True,
-                    value = text_years)
-                em.add_field(
-                    name = 'Days',
-                    inline = True,
-                    value = text_days)
-                em.add_field(
-                    name = 'Hours',
-                    inline = True,
-                    value = text_hours)
-                em.add_field(
-                    name = 'Minutes',
-                    inline = True,
-                    value = text_minutes)
+                    value = '\n'.join(text_times))
                 
                 em.set_author(
                     name = 'Server Times', 
@@ -280,17 +253,14 @@ class PlaytimeCommand(BaseCommand):
 
                 await self.bot.send_message(message.channel, embed = em)
             else:
-                print('Server Times')
-                print('Name: Years, Days, Hours, Minutes')
+                print('Name: Time')
                 
-                for item in zip(text_names, text_years, text_days, text_hours, text_minutes):
-                    print(item[0] + ': ' + item[1] + ', ' + item[2] + ', ' + item[3] + ', ' + item[4])
+                for item in zip(text_names, text_times):
+                    print(item[0] + ': ' + item[1])
         elif len(args) >= 1:
+            # Process args as player names
             try:
                 players = []
-                stat_id = 'stat.playOneMinute'
-                total_minutes = 0
-                total_ticks = 0
 
                 async with CommandCache.semaphore:
                     for arg in args:
@@ -303,99 +273,13 @@ class PlaytimeCommand(BaseCommand):
                         player_name = self.cache.names[self.cache.lower_names.index(lower_name)]
 
                         players.append(PlayerInfo(player_name, uuid))
-                
-                for player in players:
-                    file = os.path.join(self.stat_folder, player.uuid + '.json')
 
-                    if not os.path.exists(file):
-                        continue
-
-                    with open(file) as json_data:
-                        stats = json.load(json_data)
-
-                    if not stat_id in stats:
-                        continue
-
-                    stat_value = stats[stat_id]
-                    
-                    player.setPlaytime(stat_value)
-
-                    total_ticks += stat_value
-
-                if players != []:
-                    total_minutes = ticksToMinutes(total_ticks)
-
-                    # Sort players in descending order based on total playtime in minutes.
-                    players = sorted(players, key = lambda p: p.played_minutes, reverse = True)
-
-                    temp_player = PlayerInfo(PlaytimeCommand.divider_player_name, '')
-                    temp_player.setPlaytime(0)
-
-                    players.append(temp_player)
-
-                    temp_player = PlayerInfo('Total', '')
-                    temp_player.setPlaytime(total_ticks)
-
-                    players.append(temp_player)
+                await self.processPlayers(players)
             except:
                 if self.bot:
                     await self.bot.send_message(message.channel, 'No playerfile or stat found')
                 else:
                     print('No playerfile or stat found')
-
-                return
-
-            if players == []:
-                if self.bot:
-                    await self.bot.send_message(message.channel, 'No playerfile or stat found')
-                else:
-                    print('No playerfile or stat found')
-            else:
-                player_names, years, days, hours, minutes = zip(*list((formatNameForEmbed(p.player_name), p.years, p.days, p.hours, p.minutes) for p in players))
-                
-                if self.bot:
-                    em = discord.Embed(
-                        description = '',
-                        colour = 0x003763)
-
-                    em.add_field(
-                        name = 'Player',
-                        inline = True,
-                        value = '\n'.join(player_names))
-                    em.add_field(
-                        name = 'Years',
-                        inline = True,
-                        value = '\n'.join(years))
-                    em.add_field(
-                        name = 'Days',
-                        inline = True,
-                        value = '\n'.join(days))
-                    em.add_field(
-                        name = 'Hours',
-                        inline = True,
-                        value = '\n'.join(hours))
-                    em.add_field(
-                        name = 'Minutes',
-                        inline = True,
-                        value = '\n'.join(minutes))
-                            
-                    em.set_author(
-                        name = 'Total Playtime', 
-                        icon_url = 'https://cdn.discordapp.com/icons/336592624624336896/31615259cca237257e3204767959a967.png')
-                    em.set_footer(text = 'Total: {:,} minutes'.format(total_minutes))
-
-                    await self.bot.send_message(message.channel, embed = em)
-                else:
-                    print('Total Playtime')
-                    print('Player: Years, Days, Hours, Minutes')
-                    
-                    for p in players:
-                        if p.player_name == PlaytimeCommand.divider_player_name:
-                            print(p.player_name)
-                        else:
-                            print(p.player_name + ': ' + p.years + ', ' + p.days + ', ' + p.hours + ', ' + p.minutes)
-
-                    print('Total: {:,} minutes'.format(total_minutes))
         else:
             if self.bot:
                 await self.bot.send_message(message.channel, 'Invalid syntax')
